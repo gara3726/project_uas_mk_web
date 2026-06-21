a59573d5fcc6b79b0899e3104da1f549d74ff31f
Nugraha <nugraha.3726@gmail.com>
2026-06-21 15:25:47 +0800

    pengumpulan kerja tugas di yotta minggu 21 juni 2026 dengan anggota yang hadir nugraha,obby,sisil,dan charles

---
+++ aset/js/script.js
@@ -0,0 +1,23 @@
+// countdown
+const countdownDate = new Date("2026-07-26T18:00:00");
+const daysElement = document.querySelector(".days");
+const hoursElement = document.querySelector(".hours");
+const minutesElement = document.querySelector(".minutes");
+const secondsElement = document.querySelector(".seconds");
+
+const updateCountdown = () => {
+  const now = new Date();
+  const timeLeft = countdownDate - now;
+
+  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  daysElement.textContent = days.toString().padStart(2, "0");
+  hoursElement.textContent = hours.toString().padStart(2, "0");
+  minutesElement.textContent = minutes.toString().padStart(2, "0");
+  secondsElement.textContent = seconds.toString().padStart(2, "0");
+};
+
+setInterval(updateCountdown, 1000);
